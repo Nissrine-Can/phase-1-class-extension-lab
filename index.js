bb868ca6770a1aec6c4473e0b6f419afeddd2318
Nissrine Canina <niss.canina@gmail.com>
2021-10-06 18:17:38 -0400

    commit tree 146fc71018f7b5bb8830b4a6f4246c9390874de0

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+
+let i = 0
+
+class Polygon {
+    constructor(sideArr) {
+        this.side = sideArr[i]
+    }
+    get countSides() {
+      return sideArr.length
+      
+    }
+    get perimeter() {
+        reducer = (prevEl, currEl) => prevEl + currEl
+        initialValue = 0
+        return sideArr.reduce(reducer, initialValue)
+
+    }
+}
+class Triangle extends Polygon {
+    get isValid() {
+     for (let  i=0; i<sideArr.length; i++) {
+        if (sideArr[i] + sideArr[i+1] > sideArr[i+2]) {
+            return true
+        } else {
+            return false
+        }
+     }
+    
+      
+    }
+}
+class Square extends Polygon {
+    get isValid() {
+     if (perimeter % 4 === 0) {
+         return true
+     }
+    }
+    get area() {
+         return this.side * this.side
+    }
+}
